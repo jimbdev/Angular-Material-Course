0695217a8ffae5afdb35abfe171697a9af7b3776
jimbdev <jimbdev@hotmail.com>
2018-11-30 14:12:16 -0700

    Just before adding datepicker

---
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
+import { User } from '../../models/user';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-contact-dialog',
+  templateUrl: './new-contact-dialog.component.html',
+  styleUrls: ['./new-contact-dialog.component.scss']
+})
+export class NewContactDialogComponent implements OnInit {
+
+  avatars = [
+    'svg-1', 'svg-2', 'svg-3', 'svg-4'
+  ];
+
+  user: User;
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
+
+  name = new FormControl('', [Validators.required]);
+
+  getErrorMessage() {
+    return this.name.hasError('required') ? 'You must enter a name' : '';
+  }
+
+  ngOnInit() {
+    this.user = new User();
+  }
+
+  save() {
+    this.dialogRef.close(this.user);
+  }
+
+  dismiss() {
+    this.dialogRef.close(null);
+  }
+}
